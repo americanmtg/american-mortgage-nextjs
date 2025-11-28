0f1ccadbebf90e965c1fb0332a7c91d1a741ad67
americanmtg <preston@americanmtg.com>
2025-11-27 19:20:18 -0600

    Update sanity.config.ts

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -12,31 +12,10 @@ export default defineConfig({
   
   projectId,
   dataset,
+  basePath: '/studio',
   
   plugins: [
-    structureTool({
-      structure: (S) =>
-        S.list()
-          .title('Content')
-          .items([
-            S.listItem()
-              .title('Site Settings')
-              .child(S.document().schemaType('siteSettings').documentId('siteSettings')),
-            S.listItem()
-              .title('Homepage')
-              .child(S.document().schemaType('homepage').documentId('homepage')),
-            S.divider(),
-            S.listItem()
-              .title('Blog Posts')
-              .child(S.documentTypeList('blogPost').title('Blog Posts')),
-            S.listItem()
-              .title('Authors')
-              .child(S.documentTypeList('author').title('Authors')),
-            S.listItem()
-              .title('Categories')
-              .child(S.documentTypeList('category').title('Categories')),
-          ]),
-    }),
+    structureTool(),
     visionTool(),
   ],
   
