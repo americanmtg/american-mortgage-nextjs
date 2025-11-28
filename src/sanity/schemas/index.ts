87891d5105cfb81d6aa6a6a34b544882c995f5ec
americanmtg <preston@americanmtg.com>
2025-11-27 18:53:01 -0600

    Create index.ts

---
+++ src/sanity/schemas/index.ts
@@ -0,0 +1,13 @@
+import blogPost from './blogPost';
+import author from './author';
+import category from './category';
+import homepage from './homepage';
+import siteSettings from './siteSettings';
+
+export const schemaTypes = [
+  blogPost,
+  author,
+  category,
+  homepage,
+  siteSettings,
+];
